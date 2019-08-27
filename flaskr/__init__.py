fce42a16755a606cbc61ba67b5eedaa320a2a98e
Rahul <asdasdada@fm,.l.l>
2019-08-27 13:01:30 +0530

    setup steps for flask and mysqlalchemy

---
+++ flaskr/__init__.py
@@ -0,0 +1,39 @@
+import os
+from flask import Flask
+from . import db
+
+
+def create_app(test_config=None):
+    #create and configure app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE = os.path.join(app.instance_path, 'flaskr.sqlite')
+    )
+    if test_config is None:
+        #load instance config
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        #load test config
+        app.config.from_mapping(test_config)
+
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    db.init_app(app)
+
+    return app
+
+"""
+
+    @app.route('/hello')
+    def hello():
+        return 'Hello'
+
+    return app
+
+"""
+
